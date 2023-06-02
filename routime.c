ff96d46cfd2d0eb297ad90b3bc354dda438fbfbb
Miguel <miguelcarrelopaulino@gmail.com>
2023-06-02 12:40:04 +0100

    case one and print fix

--- routime.c
+++ routime.c
@@ -15,16 +15,16 @@
 void	print_status(t_general *general, int id, char c)
 {
 	pthread_mutex_lock(&(*general).print);
-	if (c == 'e')
+	if (c == 'e' && !(*general).is_dead)
 		printf("%d ms %d is eating\n", get_time() - (*general).day_time,
 			(*general).philo[id].id);
-	else if (c == 'f')
+	else if (c == 'f' && !(*general).is_dead)
 		printf("%d ms %d has taken a fork \n", get_time() - (*general).day_time,
 			(*general).philo[id].id);
-	else if (c == 't')
+	else if (c == 't' && !(*general).is_dead)
 		printf("%d ms %d is thinking\n", get_time() - (*general).day_time,
 			(*general).philo[id].id);
-	else if (c == 's')
+	else if (c == 's' && !(*general).is_dead)
 		printf("%d ms %d is sleeping\n", get_time() - (*general).day_time,
 			(*general).philo[id].id);
 	pthread_mutex_unlock(&(*general).print);
@@ -46,6 +46,14 @@ void	plswork(t_general *general, int id)
 	usleep(100);
 }
 
+void	one_philo(t_general *general, int id)
+{
+	print_status(general, id, 'f');
+	(*general).philo[id].last_eat = 0;
+	usleep((*general).die_time * 1000);
+	philo_is_dead(general, id);
+}
+
 void	*routime(void *arg)
 {
 	t_general	*general;
@@ -53,6 +61,8 @@ void	*routime(void *arg)
 
 	general = (t_general *)arg;
 	id = (*general).helper;
+	if ((*general).philos_nbr == 1)
+		one_philo(general, id);
 	if ((*general).must_eat != -1)
 	{	
 		pthread_mutex_lock(&(*general).print);
